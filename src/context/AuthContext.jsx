9cb2199d59d40cfb9ae3e5d17ee0222b6e786dc8
Don Soby <donkiran2255@gmail.com>
2026-07-17 15:37:24 +0530

    feat: implement AuthContext for user authentication and wrap App component with AuthProvider

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,54 @@
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase/config";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  function signUp(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function logOut() {
+    return signOut(auth);
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  value = {
+    currentUser,
+    loading,
+    signUp,
+    login,
+    logOut,
+  };
+
+  return (
+    <AuthContextProvider value={value}>
+      {!loding && children}
+    </AuthContextProvider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
