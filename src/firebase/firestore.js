31908277ff7fa552d68ceeff437c3f3029935f52
Don Soby <donkiran2255@gmail.com>
2026-07-18 13:14:00 +0530

    feat: implement Firestore functions for creating, retrieving, updating, and deleting posts

---
+++ src/firebase/firestore.js
@@ -0,0 +1,61 @@
+import { addDoc, collection, deleteDoc, orderBy, query, serverTimestamp, updateDoc } from "firebase/firestore"
+import { db } from "./config"
+
+
+const POSTS_COLLECTION = "posts"
+
+export async function createPost(title, content, authorId, authorName) {
+    try{
+        const docRef = await addDoc(collection(db, POSTS_COLLECTION),
+        {
+            title, 
+            content, 
+            authorId, 
+            authorName, 
+            createdAt:serverTimestamp()
+        })
+        return docRef.id
+    
+    }catch(err){
+        console.error("Error while creating post: ", err)
+        throw err
+    }
+}
+
+export async function getPosts() {
+    try{
+
+        const q = query(collection(db, POSTS_COLLECTION), orderBy("createdAt", "desc"))
+        const querySnapshot = await getDocs(q)
+
+        const posts = querySnapshot.docs.map(doc => ({
+            id:doc.id,
+            ...doc.data()
+        }))
+
+        return posts
+    }catch(err){
+        console.error("Error while getting posts: ", err);
+        throw err
+    }
+}
+
+export async function updatePost(postId, updatedData) {
+    try{
+        const postRef = doc(db, POSTS_COLLECTION, postId)
+        await updateDoc(postRef, updatedData)
+    }catch(err){
+        console.error("Error while updating post: ", err);
+        throw new err
+    }
+}
+
+export async function deletePost(postId) {
+    try{
+        const postRef = doc(db, POSTS_COLLECTION, postId)
+        await deleteDoc(postRef)
+    }catch(err){
+        console.error("Error while deleting post: ", err);
+        throw err
+    }
+}
