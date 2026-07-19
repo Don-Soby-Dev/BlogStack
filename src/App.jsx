c138b90eba6ffbc41c43e841585589c2d262ea05
Don Soby <donkiran2255@gmail.com>
2026-07-19 20:09:16 +0530

    feat: implement lazy loading for AuthPage, BlogDetail, and AddEditBlog components

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,42 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import AuthPage from "./pages/AuthPage";
 import BlogList from "./pages/BlogList";
-import AddEditBlog from "./pages/AddEditBlog";
-import BlogDetail from "./pages/BlogDetail";
 import BlogsLayout from "./components/layout/BlogsLayout";
 import ProtectedRoute from "./components/layout/ProtectedRoute";
+import { lazy, Suspense } from "react";
+
+const AuthPage = lazy(() => import("./pages/AuthPage"));
+const BlogDetail = lazy(() => import("./pages/BlogDetail"));
+const AddEditBlog = lazy(() => import("./pages/AddEditBlog"));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/blogs" replace />} />
-      <Route path="/login" element={<AuthPage />} />
-      <Route path="/signup" element={<AuthPage />} />
-      <Route path="/blogs" element={<BlogsLayout />}>
-        <Route index element={<BlogList />} />
-        <Route path=":id" element={<BlogDetail />} />
-        <Route
-          path="new"
-          element={
-            <ProtectedRoute>
-              <AddEditBlog />{" "}
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="edit/:id"
-          element={
-            <ProtectedRoute>
-              <AddEditBlog />{" "}
-            </ProtectedRoute>
-          }
-        />
-      </Route>
-    </Routes>
+    <Suspense fallback={<h1>Loading..</h1>}>
+      <Routes>
+        <Route path="/" element={<Navigate to="/blogs" replace />} />
+        <Route path="/login" element={<AuthPage />} />
+        <Route path="/signup" element={<AuthPage />} />
+        <Route path="/blogs" element={<BlogsLayout />}>
+          <Route index element={<BlogList />} />
+          <Route path=":id" element={<BlogDetail />} />
+          <Route
+            path="new"
+            element={
+              <ProtectedRoute>
+                <AddEditBlog />{" "}
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="edit/:id"
+            element={
+              <ProtectedRoute>
+                <AddEditBlog />{" "}
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
